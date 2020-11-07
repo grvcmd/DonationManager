2cf2edace0adb8fd598cee3d8f0720e8d1fcda9e
Ken Valverde <verify>
2020-11-07 13:57:16 -0600

    - Implement List Donations feature to index.html

--- src/main/java/com/CIS3368/DonationManager/DonationController.java
+++ src/main/java/com/CIS3368/DonationManager/DonationController.java
@@ -1,4 +1,23 @@
 package com.CIS3368.DonationManager;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
 public class DonationController {
+    @Autowired
+    private DonationService service;
+
+    @RequestMapping("/")
+    public String viewHomePage(Model model) {
+        // Show list of current donations
+        List<Donation> listDonations = service.listAll();
+        model.addAttribute("listDonations", listDonations);
+
+        return "index";
+    }
 }
